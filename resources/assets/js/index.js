fc2d30bffff3decbd3d844d596b9e82b947abc79
imballinst <try.aji@efishery.com>
2017-10-09 09:28:13 +0700

    chore(precommit): added pre-commit

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import CInput from './CInput';
 // SCSS
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import '../scss/index.scss';
+// Relatifve imports
+import CInput from './CInput';
 
 render(
   <CInput />,
